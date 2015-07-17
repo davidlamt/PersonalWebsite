ca5cc0d8c3435efd16a61f7fd1f26639544fcdff
David Tran <davidlamt@gmail.com>
2015-07-17 16:35:00 -0700

    Added function to the 'FIND OUT MORE' button and smooth scrolling.

--- creative.js
+++ creative.js
@@ -1,2 +1,19 @@
 // Sets the background image's height to the window's height
 $(".contentContainer").css("min-height", $(window).height());
+
+// Performs smooth scrolling to an anchor on the same page
+// Found online
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
