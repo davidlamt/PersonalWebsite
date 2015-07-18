f49d194c1efa3f994cd3a60c1d74c6ab5081d582
David Tran <davidlamt@gmail.com>
2015-07-17 18:00:12 -0700

    Added skills section.

--- creative.js
+++ creative.js
@@ -2,7 +2,7 @@
 $(".contentContainer").css("min-height", $(window).height());
 
 // Performs smooth scrolling to an anchor on the same page
-// Found online
+// Found the following code online
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
