4da7245d0d5a14720bf87b1c5d7699a2921e18e2
David Tran <davidlamt@gmail.com>
2015-07-17 19:02:26 -0700

    Added a toast message for when the site loads.

--- creative.js
+++ creative.js
@@ -17,3 +17,26 @@ $(function() {
     }
   });
 });
+
+// Performs a toast when the page is loaded
+// Found the following code online
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": true,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": true,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "4000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
+$(window).load(function () {
+  toastr.info('Hi there!<br />This site is still under construction.')
+});
